0108fdb72a13b0e4d5bad8d86fd5c33ee08dab6e
hasan <hhcadirli@gmail.com>
2023-12-26 16:05:06 +0300

    Otel Sistemi

---
+++ SenOtelFr/SenOtelFr/Form8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SenOtelFr
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form4 donus= new Form4();
+            donus.Show();
+            this.Hide();
+        }
+    }
+}
